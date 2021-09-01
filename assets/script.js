0bd7a6a0a374eac5140b2faf3dd47cbb9b23bf0c
Jonathan Bokungu <jonathanbokungu21@gmail.com>
2021-09-01 13:54:08 -0400

    updated script

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
 var searchButtonEl = document.querySelector("#click");
+var word = "dog"
 
-var wordApi = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+var wordApi = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
 
 fetch(wordApi)
 .then(function(response)  {
     return response.json();
 })
 .then(function(data) {
-    console.log(data);
+    console.log(data[0].meanings[0].definitions[0].definition);
+    console.log(data[0].meanings[0].definitions[0].synonyms);
+    console.log(data[0].meanings[0].definitions[0].example);
+    console.log(data[0].origin);
 })
 
 
@@ -34,4 +38,3 @@ fetch(wordApi)
 
 
 
-var 
