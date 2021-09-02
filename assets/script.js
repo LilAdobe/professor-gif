2f523de3f1c4aa2fe8b768c802b237efb5bfb932
Jonathan Bokungu <jonathanbokungu21@gmail.com>
2021-09-02 10:12:40 -0400

    i populated the weboite with data from the api

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
 var searchButtonEl = document.querySelector("#click");
-var word = "dog"
+var wordTitleEl = document.querySelector("#wordTitle");
+var wordDefinitionEl = document.querySelector("#wordDefinition");
+var wordSynonymsEl = document.querySelector("#wordSynonyms");
+var wordOriginEl = document.querySelector("#wordOrigin");
+var wordExampleEl = document.querySelector("#wordExample");
+// var word = "Dog"
 
 
 
@@ -10,19 +15,50 @@ var gifApiKEY = "RZpOWDlrvuEl5BafWnBkzvpjjuVEaDra"
 
 //
 
-var wordApi = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+function search(e) {
+    e.preventDefault()
+    var word = document.getElementById("wordInput").value
+    var wordApi = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
 
-fetch(wordApi)
-.then(function(response)  {
-    return response.json();
-})
-.then(function(data) {
-    getGiphy(word);
-    console.log(data[0].meanings[0].definitions[0].definition);
-    console.log(data[0].meanings[0].definitions[0].synonyms);
-    console.log(data[0].meanings[0].definitions[0].example);
-    console.log(data[0].origin);
-})
+    fetch(wordApi)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            getGiphy(word);
+            console.log(data[0].meanings[0].definitions[0].definition);
+            console.log(data[0].meanings[0].definitions[0].synonyms);
+            console.log(data[0].meanings[0].definitions[0].example);
+            console.log(data[0].origin);
+
+            wordTitleEl.textContent = word + ":";
+            wordDefinitionEl.textContent = data[0].meanings[0].definitions[0].definition
+           // wordSynonymsEl.textContent = data[0].meanings[0].definitions[0].synonyms
+            //wordOriginEl.textContent = data[0].origin
+
+            if (!data[0].meanings[0].definitions[0].synonyms) {
+                wordSynonymsEl.textContent = "No synonym available"
+            } else {
+                wordSynonymsEl.textContent = data[0].meanings[0].definitions[0].synonyms
+            }
+
+            if (!data[0].meanings[0].definitions[0].example) {
+                wordExampleEl.textContent = "No example available"
+            } else {
+                wordExampleEl.textContent = data[0].meanings[0].definitions[0].example
+            }
+
+            if (!data[0].origin) {
+                wordOriginEl.textContent = "No origin available"
+            } else {
+                wordOriginEl.textContent = data[0].origin
+            }
+        })
+}
+
+
+searchButtonEl.addEventListener("click", search)
 
 
 
@@ -30,7 +66,9 @@ fetch(wordApi)
 
 
 
- // Alexis - gif vars and functions
+
+
+// Alexis - gif vars and functions
 //gif var
 var gifSearchWord = document.querySelector("#click");
 //gif var
@@ -41,7 +79,7 @@ var gif1InputEl = document.querySelector('#gif1')
 
 var getGiphy = function (gif) {
 
-    var gifUrl = 'https://api.giphy.com/v1/gifs/search?q=' + gif + "&api_key=" + gifApiKEY +"&limit=10"+"&rating=pg";
+    var gifUrl = 'https://api.giphy.com/v1/gifs/search?q=' + gif + "&api_key=" + gifApiKEY + "&limit=10" + "&rating=pg";
 
 
 
@@ -62,12 +100,12 @@ var getGiphy = function (gif) {
 
 var displaygif = function (gifData, gifName) {
     if (gifData.length === 0) {
-        weatherContainerEL.textContent= "no gif found :(";
+        weatherContainerEL.textContent = "no gif found :(";
         return;
     }
 
     console.log(gifData)
-gifSearchWord.textContent = gifName
+    // gifSearchWord.textContent = gifName
 
 
 }
